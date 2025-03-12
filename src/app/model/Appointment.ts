54325821e1d2545fb330f86711bc11594a31e017
mohamed tergui <simotergui4@gmail.com>
2025-03-12 12:34:29 +0000

    change the appointment model

--- src/app/model/Appointment.ts
+++ src/app/model/Appointment.ts
@@ -3,6 +3,8 @@ import { AppointmentStatus } from "./AppointmentStatus";
 
 export interface Appointment {
     id: string;
+    title:string;
+    description:string
     startDateTime: string;
     endDateTime: string;
     patientId:string;
